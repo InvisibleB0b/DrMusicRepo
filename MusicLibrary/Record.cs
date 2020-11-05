fabe6be6c181ce0750fb43be68f3234e16fd0efe
InvisibleB0b <theodor.per@gmail.com>
2020-11-05 09:42:51 +0100

    Opsætning

---
+++ MusicLibrary/Record.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MusicLibrary
+{
+    public class Record
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public int Playtime { get; set; }
+        public int ReleaseYear { get; set; }
+
+        public Record()
+        {
+
+        }
+
+        public Record(string title, string artist, int playtime, int releaseYear)
+        {
+            Title = title;
+            Artist = artist;
+            Playtime = playtime;
+            ReleaseYear = releaseYear;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Title)}: {Title}, {nameof(Artist)}: {Artist}, {nameof(Playtime)}: {Playtime}, {nameof(ReleaseYear)}: {ReleaseYear}";
+        }
+    }
+}
